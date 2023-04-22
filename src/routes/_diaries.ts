cc78ee01a9f3907f06d2849fbb68a397a51a42b0
erikaArango <erikaarango.developer@gmail.com>
2023-04-22 06:22:01 -0400

    get method

--- src/routes/_diaries.ts
+++ src/routes/_diaries.ts
@@ -1,13 +1,19 @@
-import express  from "express"
+import express from 'express'
+import * as diaryServices from '../services/diaryServices'
 
 const router = express.Router()
 
-router.get('/', (_req,res) => {
-    res.send('fetching allentry diaries')
+router.get('/', (_req, res) => {
+  res.send(diaryServices.getEntriesWithoutSensitiveInfo())
 })
 
-router.post ('/', (_req,res) => {
-    res.send('saving a diary!')
+router.get('/:id', (req, res) => {
+  const diary = diaryServices.findById(+req.params.id)
+  res.send(diary?.weather)
+})
+
+router.post('/', (_req, res) => {
+  res.send('saving a diary!')
 })
 
 export default router
